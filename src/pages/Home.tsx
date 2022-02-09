58c55d6258db6604193d85c9c8b630f3c230adc5
Shailendra Kumar PALLIKONDA <s.pallikonda@f5.com>
2022-02-09 18:35:30 +0530

    Initial commit.

---
+++ src/pages/Home.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+type HomeProps = {};
+
+export default function Home({}: HomeProps) {
+  return <div>Home page</div>;
+}
---
+++ tailwind.config.js
@@ -0,0 +1,7 @@
+module.exports = {
+  content: ["./node_modules/flowbite/**/*.js", "./src/**/*.{js,jsx,ts,tsx}"],
+  theme: {
+    extend: {},
+  },
+  plugins: [require("flowbite/plugin")],
+};
